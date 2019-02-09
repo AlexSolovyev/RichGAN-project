bb7772759ce100757c67442bded4b7d22ae8456b
AlexeySolovyov38@yandex.ru <AlexeySolovyov38@yandex.ru>
2019-02-09 19:14:05 +0300

    update

--- starting_kit/submission_model.py
+++ starting_kit/submission_model.py
@@ -2,19 +2,53 @@ import numpy as np
 import pandas as pd
 
 class Model:
-    def fit(self, X, Y):
-        self.means = {}
-        self.stds = {}
-        for col in Y.columns:
-            self.means[col] = np.mean(Y[col])
-            self.stds [col] = np.std (Y[col])
+    def train(self, X, Y, n_bins=25):
+        self.means = dict.fromkeys(Y.columns, np.zeros((n_bins, n_bins, n_bins)))
+        self.stds = dict.fromkeys(Y.columns, np.zeros((n_bins, n_bins, n_bins)))
+        self.bins = {}
+        cols = X.columns
+        for col in cols:
+            self.bins[col] = np.percentile(X[col], 100*np.linspace(1./n_bins, (n_bins-1)/n_bins,n_bins-1))
+        self.masks = create_masks(n_bins, X, self.bins)
+        
+        for i in range(n_bins):
+            for j in range(n_bins):
+                for k in range(n_bins):
+                    f_masks = np.logical_and(self.masks[cols[0]][:,i], self.masks[cols[1]][:,j], self.masks[cols[2]][:,k]) 
+                    for col in Y.columns:
+                        self.means[col][i][j][k] = np.mean(Y[col][f_masks])
+                        self.stds[col][i][j][k] = np.std(Y[col][f_masks])
+        
 
     def predict(self, X):
         pred = pd.DataFrame()
+        count = np.zeros((self.means['RichDLLk'].shape), dtype=np.int)
+        n_bins = len(count)
+        pred_masks = create_masks(n_bins, X, self.bins)
+        cols = X.columns
+        
         for col in self.means.keys():
-            pred[col] = np.random.normal(
-                                loc=self.means[col],
-                                scale=self.stds[col],
-                                size=len(X)
-                            )
+            samples = np.array([])
+            for i in range(n_bins):
+                for j in range(n_bins):
+                    for k in range(n_bins):
+                        samples = np.append(samples,
+                                         np.random.normal(
+                                            loc=self.means[col][i][j][k],
+                                            scale=self.stds[col][i][j][k],
+                                            size=np.count_nonzero(np.logical_and(pred_masks[cols[0]][:,i],
+                                                                   pred_masks[cols[1]][:,j],
+                                                                   pred_masks[cols[2]][:,k]) )
+                                         )
+                                        )
+            pred[col] = samples
         return pred
+    
+def create_masks(count, features, bins):
+    for col in features.columns:
+        mask = np.zeros((len(features[col]), count), dtype=np.bool)
+        mask[:, 0], mask[:, -1] = features[col] <= bins[col][0], features[col] > bins[col][-1]
+        for i in range(count - 2):
+            mask[:,i+1] = (bins[col][i] < features[col]) & (features[col] <= bins[col][i+1])
+        masks[col] = mask
+    return masks
